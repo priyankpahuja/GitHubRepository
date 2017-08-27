3e014227ade1cc5f77d909cc8bc8427321708734
priyank.pahuja@gmail.com <priyank@123>
2017-08-27 21:14:27 +0530

    QR Code Added into it

---
+++ GitHubRepository/qrCode.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="qrCode.aspx.cs" Inherits="GitHubRepository.qrCode" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:TextBox ID="txtCode" runat="server"></asp:TextBox>
+        <asp:Button ID="btnGenerate" runat="server" Text="Generate" onclick="btnGenerate_Click" />
+        <hr />
+        <asp:PlaceHolder ID="plBarCode" runat="server" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ GitHubRepository/qrCode.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using QRCoder;
+using System.IO;
+using System.Drawing;
+
+namespace GitHubRepository
+{
+    public partial class qrCode : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnGenerate_Click(object sender, EventArgs e)
+        {
+            string code = txtCode.Text;
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeGenerator.QRCode qrCode = qrGenerator.CreateQrCode(code, QRCodeGenerator.ECCLevel.Q);
+            System.Web.UI.WebControls.Image imgBarCode = new System.Web.UI.WebControls.Image();
+            imgBarCode.Height = 150;
+            imgBarCode.Width = 150;
+            using (Bitmap bitMap = qrCode.GetGraphic(20))
+            {
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    bitMap.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                    byte[] byteImage = ms.ToArray();
+                    imgBarCode.ImageUrl = "data:image/png;base64," + Convert.ToBase64String(byteImage);
+                }
+                plBarCode.Controls.Add(imgBarCode);
+            }
+        }
+    }
+}
